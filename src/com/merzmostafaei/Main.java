bd801fb04c2784cdd41ae8d3c1a3fe7112cfa45f
merz mostafaei <merzmostafaei@gmail.com>
2025-05-21 11:16:07 -0200

    ComparableInterface

--- src/com/merzmostafaei/Main.java
+++ src/com/merzmostafaei/Main.java
@@ -3,6 +3,7 @@ package com.merzmostafaei;
 import com.merzmostafaei.exceptions.ExeptionDemo;
 import com.merzmostafaei.generics.GenericList;
 import com.merzmostafaei.generics.User;
+import com.merzmostafaei.generics.UserComparable;
 
 import java.io.IOException;
 
@@ -26,13 +27,24 @@ public class Main {
 //        list.add(1);
 //        int number = list.get(0);
 
-        var user = new GenericList<User>();
-        user.add(new User());
-        User indexuser = user.get(0);
+//        var user = new GenericList<User>();
+//        user.add(new User());
+//        User indexuser = user.get(0);
 
-        //Constrains, that we can extract to generic type
-        // can be object, or interface with subclasses
-        var Constraints = new GenericList<User>();
+        //--Constrains, that we can extract to generic type
+        // --can be object, or interface with subclasses
+//        var Constraints = new GenericList<User>();
+
+        // ComparableInterface
+        var user1 = new UserComparable(10);
+        var user2 = new UserComparable(20);
+        if (user1.compareTo(user2) < 0)
+            System.out.println("user1 < user2");
+        else if (user1.compareTo(user2) == 0)
+            System.out.println("user1 == user2");
+        else System.out.println("user1 > user2");
+
+
 
 
     }
---
+++ src/com/merzmostafaei/generics/UserComparable.java
@@ -0,0 +1,19 @@
+package com.merzmostafaei.generics;
+
+public class UserComparable implements Comparable<UserComparable> {
+    public int points;
+
+    public UserComparable(int points) {
+        this.points = points;
+    }
+
+    @Override
+    public int compareTo(UserComparable other) {
+        // this < 0 --> -1
+        // this == 0 --> 0
+        // this > 0 --> 1
+        return points - other.points;
+    }
+
+
+}
