b75203aef08879c23f68389981cf293b1c33db12
merz mostafaei <merzmostafaei@gmail.com>
2025-05-20 23:25:11 -0200

    Re Throw Exception

--- src/com/merzmostafaei/Main.java
+++ src/com/merzmostafaei/Main.java
@@ -1,15 +1,20 @@
+package com.merzmostafaei;
+
+import com.merzmostafaei.exceptions.ExeptionDemo;
+
+import java.io.IOException;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+//        ExeptionDemo.show();
+//        ExeptionDemo.catchException();
+//        ExeptionDemo.TryResource();
+        try {
+            ExeptionDemo.ThrowingExceptions();
+        } catch (Throwable e) {
+            System.out.println("An unexpected error occurred");
         }
     }
 }
--- src/com/merzmostafaei/exceptions/ExeptionDemo.java
+++ src/com/merzmostafaei/exceptions/ExeptionDemo.java
@@ -3,6 +3,7 @@ package com.merzmostafaei.exceptions;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.Writer;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
@@ -15,35 +16,69 @@ public class ExeptionDemo {
     public static void sayHello(String name){
         System.out.println(name.toUpperCase());
     }
-    public static void catchException(){
-        FileReader reader = null ; // beacuse Filereader not gonna executet
-        try
+//    public static void catchException(){
+//        FileReader reader = null ; // beacuse Filereader not gonna executet
+//        try
+//
+//        {
+//            reader = new FileReader("file.txt");
+//            System.out.println("File opend");
+//            var value = reader.read();
+//            new SimpleDateFormat().parse("");
+//            //reader.close(); if put here if in excepion not gonna excute
+//        }
+//        catch (FileNotFoundException ex) {
+//            System.out.println("File not exist");
+//            System.out.println(ex.getMessage());
+//        }
+//
+//        catch (IOException | ParseException ex) {
+//            System.out.println("File not can read");
+//            System.out.println(ex.getMessage());
+//        }
+//        finally {
+//            if (reader != null) {
+//                try {
+//                    reader.close();
+//                } catch (IOException e) {
+//                    throw new RuntimeException(e);
+//                }
+//            }
+//        }
+    public static void TryResource() {
+        try (  FileReader reader =new FileReader("file.txt");
+                FileReader reader1 = new FileReader("File.txt");
 
-        {
-            reader = new FileReader("file.txt");
-            System.out.println("File opend");
-            var value = reader.read();
-            new SimpleDateFormat().parse("");
-            //reader.close(); if put here if in excepion not gonna excute
-        }
-        catch (FileNotFoundException ex) {
-            System.out.println("File not exist");
-            System.out.println(ex.getMessage());
-        }
+                )
+            {
 
-        catch (IOException | ParseException ex) {
-            System.out.println("File not can read");
-            System.out.println(ex.getMessage());
-        }
-        finally {
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
+                System.out.println("File opend");
+                var value = reader.read();
+                new SimpleDateFormat().parse("");
+                //reader.close(); if put here if in excepion not gonna excute
+            }
+        catch(FileNotFoundException ex){
+                System.out.println("File not exist");
+                System.out.println(ex.getMessage());
+            }
+
+        catch(IOException | ParseException ex){
+                System.out.println("File not can read");
+                System.out.println(ex.getMessage());
             }
         }
+    public static void ThrowingExceptions() throws IOException {
+        var account = new Account();
+        try {
+            account.deposit(-1);
+        } catch (IOException e) {
+//            throw new RuntimeException(e);
+            System.out.println("Logging");
+//            //Re Throwing Exception
+            throw e;
+        }
+    }
+
 
     }
-}
+
