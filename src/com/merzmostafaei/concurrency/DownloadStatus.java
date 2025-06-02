29a17c39f17fff6488363bf1969b9d9cb89d6f70
merz mostafaei <merzmostafaei@gmail.com>
2025-06-01 23:43:14 -0200

    The Synchronized Keyword

--- src/com/merzmostafaei/concurrency/DownloadStatus.java
+++ src/com/merzmostafaei/concurrency/DownloadStatus.java
@@ -4,25 +4,38 @@ package com.merzmostafaei.concurrency;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
+
 public class DownloadStatus {
     private int totalBytes;
+    //private Object totalByteLock = new Object(); --> use insesd of using (this) for monitor filed
 
     //Synchoronized Lock
 
-    private Lock lock = new ReentrantLock();
+    //private Lock lock = new ReentrantLock();
 
     public int getTotalBytes() {
         return totalBytes;
     }
-
+    // use synchronized keyword like this {
+//    public synchronized void incrementTotalBytes(){
+//        //try{
+//
+//        totalBytes++;
+//
+    // }
+    //-- OR --
+//   public synchronized void incrementTotalBytes(){
+      //synchronized(this);       --> if you have one filed
+     //synchoronized(totalByteLock);       ---> first you need to intrudice one object filed and use it here
+      //totalBytes++;
+    //}
     public void incrementTotalBytes(){
-        lock.lock();
-        try{
+        //lock.lock();
+        //try{
 
             totalBytes++;
-        }
-        finally {
-            lock.unlock();
-        }
+        //}
+        //finally {
+        //    lock.unlock();}
     }
 }
